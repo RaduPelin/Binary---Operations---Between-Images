c2af07374dd994c081598c55651ca006de391047
Radu Pelin <radu.pelin7@gmail.com>
2018-02-16 13:58:18 +0200

    Add files via upload

---
+++ src/resource/MyImage.java
@@ -0,0 +1,9 @@
+package resource;
+// Interfata clasei MyBitmap
+public interface MyImage {
+    
+	int getWidth();
+	int getHeight();
+	void setWidth(byte [] width);
+	void setHeight(byte [] height);
+}
